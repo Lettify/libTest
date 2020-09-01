8d9d4692f12400f991024bc3e4bacd8af19451a9
Garoze <marcelogaroze@hotmail.com>
2020-08-31 22:08:34 -0300

    add: sendDebug().result() function

--- libTest.lua
+++ libTest.lua
@@ -2,7 +2,22 @@ function sendDebug(name, message, i)
     return {
         failed = function(name, i) print(debug.getinfo(i).short_src .. ':'..debug.getinfo(i).currentline .. ' ['..name..'] - Test has Failed!') end,
         sucess = function(name, i) print(debug.getinfo(i).short_src .. ':'..debug.getinfo(i).currentline .. ' ['..name..'] - Test has Succeed!') end,
+        result = function(condition, name, i) if condition == true then sendDebug().sucess(name, i) else sendDebug().failed(name, i) end end,
     }
+end
+
+function functionHasReturn(func)
+   local d = string.dump(func)
+   assert(d:sub(1,5) == "\27LuaQ") 
+   d = d:match"^.-\30%z\128%z"
+   for pos = #d % 4 + 1, #d, 4 do
+      local b1, b2, b3, b4 = d:byte(pos, pos+3)
+      local dword = b1 + 256 * (b2 + 256 * (b3 + 256 * b4))
+      local OpCode, BC = dword % 64, math.floor(dword/16384)
+      local B, C = math.floor(BC/512), BC % 512
+      if OpCode == 30 and C == 0 and B ~= 1 then return true end
+   end
+   return false
 end
 
 function createTests(name, array)
@@ -24,16 +39,16 @@ function describe(name, func)
     } 
 end
 
-function expected(name,func)
-    local f = type(func) == 'function' and func() or func
+function expected(name, func, ...)
+    local f = type(func) == 'function' and func(...) or func
     return {
-        toBe = function(n)
-            if f == n then 
-                sendDebug().sucess(name, 3)
-            else 
-                sendDebug().failed(name, 3)
-            end 
-        end,
+        toBe = function(n) sendDebug().result(f == n, name, 3) end,
+        toReturn = function(n) sendDebug().result(f == n, name, 3) end,
     }
 end
 
+function sum(a,b) 
+    return a + b
+end
+
+print(expected('sum', sum, 1, 3).toReturn(3))

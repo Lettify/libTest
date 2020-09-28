71838762ab61a09abfe6e0cea7b1181be17178ab
Garoze <marcelogaroze@hotmail.com>
2020-09-28 03:43:31 -0300

    fix: interface file name

--- copy.lua
+++
@@ -1,1 +0,0 @@
-assert(type(validadeInterface) == 'table', '[Interface - '..self.__name..'] - Expected a \'table\' on #1 argument and got '..type(validadeInterface)) 
--- interface.lua
+++ interface.lua
@@ -1,56 +1,53 @@
-local callMeta = { __call = function(self, ...) return self.validate(self, ...) end }
+local callMeta = { __call = function(self, ...) return self:validate(...) end }
 
 function Interface(interface, name)
     return setmetatable({
         __name = name or 'default',
         __type = 'interfaceObject',
-        interface = interface,
-        validate = function(self, validadeInterface)
-            assert(type(validadeInterface) == 'table', '[Interface - '..self.__name..'] - Expected a \'table\' on #1 argument and got '..type(validadeInterface)) 
-            if hasSameFields(self.interface, validadeInterface) == true then 
-                for i, v in pairs(self.interface) do 
-                    if type(validadeInterface[i]) == v then 
-                        print(getFieldType(self.interface, validadeInterface))
-                    else
-                        print('Não sao do mesmo tipo')
+        interface = interface, 
+        validate = function(self, validateInterface)
+            assert(type(validateInterface) == 'table', '[Interface - '..self.__name..'] - Expected a \'table\' on #1 argument and got '..type(validadeInterface)) 
+			if self:hasSameFields(validateInterface) == true then 
+                local returnValue = 0
+                for field, v in pairs(validateInterface) do 
+                    if self:getFieldType(field, validateInterface) == false then
+                        print('Name: '..self.__name..' (f)_getDiffFields ['..field..'] - Expected \''..self.interface[field]..'\' and got '..type(validateInterface[field]))
+                        returnValue = returnValue + 1
                     end
                 end
-            else 
-                print(unpack(getDiffFields(self.interface, validadeInterface)))
+                return returnValue == 0 and true or false
+            else return false, self:getDiffFields(validateInterface) end
+        end,
+        hasSameFields = function(self, validateInterface)
+            for field, v in pairs(self.interface) do 
+                if validateInterface[field] == nil then return false end 
             end
+            return true
+        end,
+        getDiffFields = function(self, validateInterface) 
+            local index = {}
+            for field, _ in pairs(self.interface) do 
+                if validateInterface[field] == nil then table.insert(index, tostring(field)) end 
+            end
+            return index
+        end,
+        hasSameType = function(self, validateInterface) 
+            for field, t in pairs(self.interface) do 
+                if type(validateInterface[i]) ~= t then return false end
+            end
+            return true
+        end,
+        getFieldType = function(self, field, validateInterface)
+            if type(validateInterface[field]) ~= self.interface[field] then return false end  
         end,
     }, callMeta)
 end
 
-function getFieldType(t, validateField)
-    for i, v in pairs(t) do 
-        if type(validateField[i]) ~= v then 
-            return i
-        end
-    end
-    return true
-end
-
-function hasSameFields(t, validateTable)
-    for i, v in pairs(t) do 
-        if validateTable[i] == nil then return false end
-    end
-    return true
-end
-
-function getDiffFields(t, validateTable)
-    local index = {}
-    for i, v in pairs(t) do 
-        if validateTable[i] == nil then table.insert(index, tostring(i)) end
-    end
-    return #index >= 1 and index or false
-end
-
-
-local teste = Interface({
+local Person = Interface({
     name = 'string',
     age = 'number',
-})
+    hobbies = 'table',
+    male = 'boolean'
+}, 'PersonInterface')
 
-print(teste({name = 'oi', age = 23}))
-
+Person({name = 'Dev', age = 23, hobbies = {}, male = true})
